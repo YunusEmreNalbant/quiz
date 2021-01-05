7405a3a3b8bb977eadda1cef193974b0542c9a29
YunusEmreNalbant <yunus.nalbant0@gmail.com>
2021-01-05 10:59:40 +0300

    migration,factory,seed olusturuldu.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Quiz;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
@@ -17,13 +18,15 @@ class DatabaseSeeder extends Seeder
     {
 
         User::insert([
-        'name'=>'Yunus Emre Nalbant',
-        'email'=>'yunus@hotmail.com',
-        'email_verified_at' => now(),
-        'type'=>'admin',
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
-    ]);
-         \App\Models\User::factory(5)->create();
+            'name' => 'Yunus Emre Nalbant',
+            'email' => 'yunus@hotmail.com',
+            'email_verified_at' => now(),
+            'type' => 'admin',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+        \App\Models\User::factory(5)->create();
+        Quiz::factory(10)->create();
+
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,3 @@
 <x-app-layout>
     <x-slot name="header">Anasayfa</x-slot>
-    <div class="alert alert-danger">Bootstrap Alert</div>
-
 </x-app-layout>
